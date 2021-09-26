96f7c0d76b9aae73e502a211ff374c6f10b9a1d6
gjack <gabi@gabijack.com>
2021-09-26 09:32:57 -0400

    Adding a Layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 // styles
 const pageStyles = {
@@ -27,7 +27,8 @@ const codeStyles = {
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <Layout>
+      <main style={pageStyles}>
       <title>Not found</title>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
@@ -36,18 +37,9 @@ const NotFoundPage = () => {
           😔
         </span>{" "}
         we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
       </p>
     </main>
+    </Layout>
   )
 }
 
