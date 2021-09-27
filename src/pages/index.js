7b508f256adbcaee769a98cff686098c1507d7e1
gjack <gabi@gabijack.com>
2021-09-26 23:00:18 -0400

    Processing images with StaticImage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,16 @@ import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 // markup
 const IndexPage = ({data}) => {
   const posts = data.allMdx.nodes
+  
   return (
     <Layout>
       <SEO title = {"Home Page"} />
+      <StaticImage src={"../images/banner-blocks.jpg"} alt={"A banner"} layout={"fullWidth"} aspectRatio={12/2}/>
       {posts.map((post) => {
         return (
           <Link to={post.slug} key={post.slug}>
