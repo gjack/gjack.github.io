f87fd3797609a9dff90a9436e4e6a74c4b71c14d
gjack <gabi@gabijack.com>
2021-10-24 11:20:06 -0400

    Styling for blockquote is a bit ugly. Change it.

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -6,46 +6,22 @@ export default {
   pre: (props) => <div sx={{ pre: { padding: "1rem" } }}>{props.children}</div>,
   code: Prism,
   blockquote: (props) => (
-    <blockquote
+    <div
       sx={{
-        width: "80%",
-        minHeight: "5rem",
-        position: "relative",
-        border: "5px solid #328cc1",
-        backgroundColor: "rgb(50 140 193/40%)",
-        margin: "2rem auto",
-        color: "#0b3c5d",
-        ":before": {
-          position: "absolute",
-          height: "3rem",
-          content: '"\\“"',
-          top: "0.3rem",
-          left: "0.9rem",
-          font: "6rem/100% Georgia, Times New Roman, Times, serif",
-        },
-        ":after": {
-          position: "absolute",
-          height: "3rem",
-          content: '"\\”"',
-          bottom: "0.3rem",
-          right: "0.8rem",
-          font: "6rem/100% Georgia, Times New Roman, Times, serif",
-        },
+        background: "#f9f9f9",
+        borderLeft: "10px solid #ccc",
+        margin: "1.5em 10px",
+        padding: "1em 10px .1em 10px",
         em: {
           display: "block",
+          textAlign: "right",
           fontSize: "0.8em",
           marginTop: "1rem",
           textStyle: "italic",
-          textAlign: "right",
-        },
-        p: {
-          color: "#0b3c5d",
-          p: "0 4rem",
-          textAlign: "left",
         },
       }}
     >
       {props.children}
-    </blockquote>
+    </div>
   ),
 }
