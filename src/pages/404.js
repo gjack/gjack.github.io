88a4cc4d2bcc173fee50beab89ea9f25f996d039
gjack <gabi@gabijack.com>
2021-10-23 20:59:30 -0400

    Fix 404 page. Place a nice image in it

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { Image } from "theme-ui"
+import NotFound from "../images/404page.jpg"
 
 // styles
 const pageStyles = {
@@ -9,30 +11,39 @@ const pageStyles = {
 }
 const headingStyles = {
   marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+  marginBottom: "2rem",
+  maxWidth: "100%",
+  textAlign: "center",
 }
 
 const paragraphStyles = {
   marginBottom: 48,
 }
 
-
 // markup
 const NotFoundPage = () => {
   return (
     <Layout>
       <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-      </p>
-    </main>
+        <title>Not found</title>
+        <h1 style={headingStyles}>Oops! Page Not Found!</h1>
+        <Image
+          src={NotFound}
+          alt={"Books"}
+          sx={{
+            border: "1px solid #0b3c5d",
+            padding: "5px",
+            marginTop: "3rem",
+          }}
+        />
+        <p style={paragraphStyles}>
+          Sorry{" "}
+          <span role="img" aria-label="Pensive emoji">
+            😔
+          </span>{" "}
+          we couldn’t find what you were looking for.
+        </p>
+      </main>
     </Layout>
   )
 }
