31bcea9a122cf59c03c9fcab4b39cebe55da6d0d
Gabi Jack <gabi@gabijack.com>
2025-12-27 11:24:45 -0500

    Migrate homepage and static pages to Material-UI
      - Convert index.js: Use MUI Grid container/item, Card components
      - Convert responsive arrays to breakpoint objects
      - Replace Theme UI Image with Box component=img
      - Migrate reading.js and 404.js pages
      - Use MUI Typography for text elements
      - Update all sx styles to MUI theme palette

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-/** @jsx jsx */
-import { jsx } from "theme-ui"
+import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { graphql, Link } from "gatsby"
-import { Box, Image, Grid } from "theme-ui"
+import { graphql, Link as GatsbyLink } from "gatsby"
+import { Box, Grid, Card, CardContent, Link as MuiLink, Typography } from "@mui/material"
 import GJack from "../images/gabijackpic.png"
 
 // markup
@@ -13,55 +12,59 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <Seo title={"Home Page"} />
-      <Image
+      <Box
+        component="img"
         src={GJack}
-        alt={"Gabi Jack"}
+        alt="Gabi Jack"
         sx={{
-          width: [200, 300, null],
-          height: [200, 300, null],
+          width: { xs: 200, sm: 300 },
+          height: { xs: 200, sm: 300 },
           borderRadius: "50%",
-          border: "1px solid #0b3c5d",
+          border: "1px solid",
+          borderColor: "primary.main",
           padding: "5px",
           marginTop: "3rem",
         }}
       />
-      <article sx={{ my: "3rem", textAlign: "justify" }}>
-        <p>
+      <Box component="article" sx={{ my: "3rem", textAlign: "justify" }}>
+        <Typography component="p" sx={{ mb: 2 }}>
           Hello and welcome! Thank you for taking the time to visit my tiny
           corner of the web! My name is Gabi Jack, and I'm a Software Engineer
           in the greater Toledo, OH area. My daily job involves writing Ruby on
           Rails and React applications, but I also enjoy dabbling in other
           programming languages, such as C++ and Python, as well as
           experimenting with other frameworks and technologies.
-        </p>
-        <p>
+        </Typography>
+        <Typography component="p" sx={{ mb: 2 }}>
           In a previous life, before becoming a software developer, I graduated
           college with a BS in Mechanical Engineering, and worked for a few
           years doing design and optimization of thermal energy systems.
           Perhaps, this explains why I still keep a certain fondness for Math,
           Physics and all things related to computer aided mechanical design.
-        </p>
-        <p>
+        </Typography>
+        <Typography component="p" sx={{ mb: 2 }}>
           When I'm not working, I enjoy reading, knitting, growing roses, and
           spending time with my husband, our two boys and our little mini
           schnauzer. My hope is to make this a place to blog about all things
           that I'm learning and exploring, with an emphasis on software
           development. Hopefully, I may also manage to write something
           interesting from time to time.
-        </p>
-      </article>
-      <hr
+        </Typography>
+      </Box>
+      <Box
+        component="hr"
         sx={{
-          color: "#0b3c5d",
+          color: "primary.main",
           textAlign: "center",
           width: "90%",
           border: "none",
-          borderTop: "3px double #0b3c5d",
+          borderTop: "3px double",
+          borderColor: "primary.main",
           overflow: "visible",
           height: "5px",
           my: 5,
-          ":after": {
-            background: "#eaeaec",
+          "&::after": {
+            background: "background.default",
             content: '"\\270E"',
             padding: "0 4px",
             position: "relative",
@@ -69,40 +72,53 @@ const IndexPage = ({ data }) => {
           },
         }}
       />
-      <h3>Most recent posts</h3>
-      <Grid gap={4} columns={[1, 1, 2]} repeat={"fit"} sx={{ marginBottom: 5 }}>
+      <Typography variant="h3" component="h3" sx={{ mb: 3 }}>
+        Most recent posts
+      </Typography>
+      <Grid container spacing={4} sx={{ marginBottom: 5 }}>
         {posts.map((post, index) => (
-          <Box
-            key={`post-${post.slug}-${index}`}
-            sx={{
-              textAlign: "left",
-              padding: "1.25rem",
-              backgroundColor: "#328cc1",
-              borderRadius: "0.5rem",
-              boxShadow: "0px 9px 9px 1px rgb(0 0 70 / 20%)",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-around",
-              fontWeight: "bold",
-            }}
-          >
-            <h3 sx={{ textAlign: "center" }}>{post.frontmatter.title}</h3>
-            <div>{post.excerpt}</div>
-            <Link
-              to={`/blog/${post.slug}`}
-              key={post.slug}
+          <Grid item xs={12} md={6} key={`post-${post.slug}-${index}`}>
+            <Card
               sx={{
-                textAlign: "right",
-                textDecoration: "none",
-                color: "#1d2731",
-                ":hover": {
-                  color: "#d9b310",
-                },
+                textAlign: "left",
+                backgroundColor: "secondary.main",
+                borderRadius: "0.5rem",
+                boxShadow: "0px 9px 9px 1px rgb(0 0 70 / 20%)",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-around",
+                height: "100%",
               }}
             >
-              Continue reading
-            </Link>
-          </Box>
+              <CardContent>
+                <Typography
+                  variant="h5"
+                  component="h3"
+                  sx={{ textAlign: "center", fontWeight: "bold", mb: 2 }}
+                >
+                  {post.frontmatter.title}
+                </Typography>
+                <Typography component="div" sx={{ mb: 2 }}>
+                  {post.excerpt}
+                </Typography>
+                <MuiLink
+                  component={GatsbyLink}
+                  to={`/blog/${post.slug}`}
+                  sx={{
+                    textAlign: "right",
+                    textDecoration: "none",
+                    color: "text.primary",
+                    display: "block",
+                    "&:hover": {
+                      color: "warning.main",
+                    },
+                  }}
+                >
+                  Continue reading
+                </MuiLink>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
       </Grid>
     </Layout>
